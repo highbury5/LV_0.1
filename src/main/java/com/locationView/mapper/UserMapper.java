b34169d1a3ea615351376fee92df76fff03bd752
highbury5 <hzx5@hotmail.com>
2016-11-01 11:25:03 +0800

    change xiao to locationView
    11-01

---
+++ src/main/java/com/locationView/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.locationView.mapper;
+
+import com.locationView.domain.User;
+
+public interface UserMapper {
+	User findById(int id);
+}
--- src/main/java/com/xiao/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xiao.mapper;
-
-import com.xiao.domain.User;
-
-public interface UserMapper {
-	User findById(int id);
-}
